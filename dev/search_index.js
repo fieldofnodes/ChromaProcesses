aefa87de823f4f61c78a6db99a2755f5c732a5e4
Documenter.jl <documenter@juliadocs.github.io>
2024-07-29 22:52:55 +0000

    build based on 353163c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ChromaProcesses","category":"page"},{"location":"#ChromaProcesses","page":"Home","title":"ChromaProcesses","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ChromaProcesses.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ChromaProcesses]","category":"page"}]
+}
